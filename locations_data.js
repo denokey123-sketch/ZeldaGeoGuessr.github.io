056418773762e34206bb0db7d6cf134e9c3dade7
GDA <denokey123@gmail.com>
2026-03-24 15:13:40 +0300

    Add files via upload

---
+++ locations_data.js
@@ -0,0 +1,37 @@
+const LOCATIONS_DATA = {
+    "series": [
+        {
+            "id": 1,
+            "name": "Игра 1: Достопримечательности",
+            "rounds": [
+                { "image": "location_s01_i01.png", "x": 4500, "y": 3800 },
+                { "image": "location_s01_i02.png", "x": 7800, "y": 11800 },
+                { "image": "location_s01_i03.png", "x": 2100, "y": 6400 },
+                { "image": "location_s01_i04.png", "x": 9300, "y": 8200 },
+                { "image": "location_s01_i05.png", "x": 14200, "y": 4100 }
+            ]
+        },
+        {
+            "id": 2,
+            "name": "Игра 2: Не так просто",
+            "rounds": [
+                { "image": "location_s02_i01.png", "x": 3200, "y": 1500 },
+                { "image": "location_s02_i02.png", "x": 11000, "y": 9700 },
+                { "image": "location_s02_i03.png", "x": 5500, "y": 7200 },
+                { "image": "location_s02_i04.png", "x": 8800, "y": 3100 },
+                { "image": "location_s02_i05.png", "x": 15000, "y": 12000 }
+            ]
+        },
+        {
+            "id": 3,
+            "name": "Игра 3: Сложные локации",
+            "rounds": [
+                { "image": "location_s03_i01.png", "x": 16500, "y": 12300 },
+                { "image": "location_s03_i02.png", "x": 1200, "y": 800 },
+                { "image": "location_s03_i03.png", "x": 10500, "y": 14200 },
+                { "image": "location_s03_i04.png", "x": 6700, "y": 5500 },
+                { "image": "location_s03_i05.png", "x": 13800, "y": 9900 }
+            ]
+        }
+    ]
+};
